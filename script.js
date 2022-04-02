d4a72ec6c89d5f46b615a34bb89193268c3625e4
Edith-guru <edithnesa@gmail.com>
2022-04-03 02:48:56 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// When the user clicks the purchase button, render out
+// "Something went wrong, please try again" in the paragraph
+// that has the id="error".
+
+let errorPara = document.getElementById("error");
+
+function purchase(){
+    errorPara.textContent = "Something went wrong, please try again";
+}
